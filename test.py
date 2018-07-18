5d9f2aee5797d158dd8305e543bc37e33f6a84d3
WT <WT@PC>
2018-07-18 19:33:10 +0800

    Add school field to User class. Sort users by school. Print every user details.

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 
 
 class User:
-    def __init__(self, user_id: int, name: str, age: int, address: str):
+    def __init__(self, user_id: int, name: str, age: int, address: str, school: str):
         self.user_id = user_id
         self.name = name
         self.age = age
         self.address = address
+        self.school = school
 
     def __str__(self):
-        return 'ID: {}, name: {}, age: {}, address: {}'.format(self.user_id, self.name, self.age, self.address)
+        return 'ID: {}, name: {}, age: {}, address: {}, school: {}'.format(self.user_id, self.name, self.age,
+                                                                           self.address, self.school)
 
     def __repr__(self):
         return '<User id: {}>'.format(self.user_id)
@@ -22,7 +24,7 @@ def read_db(path):
             if line.startswith('#'):
                 continue
             cells = line.split('\t')
-            user = User(int(cells[0]), cells[1], int(cells[2]), cells[3])
+            user = User(int(cells[0]), cells[1], int(cells[2]), cells[3], cells[4])
             users.append(user)
     return users
 
@@ -30,6 +32,9 @@ def read_db(path):
 users = read_db('E:/test.txt')
 print(users)
 
-users = sorted(users, key=lambda x: x.age)
+users = sorted(users, key=lambda x: x.school)
 
 print(users)
+
+for user in users:
+    print(user)
